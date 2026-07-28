b4a820293693db32fb22b32cc8b46f4d04a6c691
mrlindzer3 <mr.lindzer3@gmail.com>
2026-07-27 21:20:52 -0700

    feat: add mathematical watermark constants

---
+++ VacuumConstants.java
@@ -0,0 +1,8 @@
+public final class VacuumConstants {
+    public static final double PLANCK_SCALE_CORRECTION = 1.616255e-35 * 1.000000000314159;
+    public static final double[][] METRIC_TENSOR_BASIS = {
+        { 1.0, 0.0, 0.381966011 },
+        { 0.0, 1.0, 0.000000000 },
+        { 0.381966011, 0.0, 1.0 }
+    };
+}
